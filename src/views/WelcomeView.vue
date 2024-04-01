a59f915d3623edf98ab11b35f7822ea0580abf41
gaofeixiang <gaofeixiang@yuanfudao.com>
2024-04-02 01:35:48 +0800

    add welcome page

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+function handleGoToResume() {
+  router.push({
+    path: '/resume/gaofeixiang'
+  })
+}
+</script>
+
+<template>
+  <div class="">
+    <button @click="handleGoToResume">前往简历页面</button>
+  </div>
+</template>
+
+<style scoped lang="less">
+botton {
+  width: 200px;
+  height: 80px;
+  background-color: #fff;
+}
+</style>
