ea20714c6e664cc42e194c318a480dc37b9da92f
gaofeixiang <gaofeixiang@yuanfudao.com>
2024-04-02 01:19:07 +0800

    change outDir

--- vite/vite.production.ts
+++ vite/vite.production.ts
@@ -1,10 +1,6 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import { baseConfig } from './vite.config'
 
 export default defineConfig({
-  ...baseConfig,
-  build: {
-    outDir: path.join(__dirname, '../docs')
-  }
+  ...baseConfig
 })
